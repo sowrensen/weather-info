47ab88f780df7e02058df8cb50386a54d5f83435
Sowren Sen <sowren@coveredpress.com>
2021-08-28 00:04:56 +0600

    register user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,15 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(): JsonResponse
+    public function register(Request $request): JsonResponse
     {
-        return \Response::json([]);
+        $input = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $token = $user->createToken(time())->plainTextToken;
+
+        return \Response::json([
+            'status' => true,
+            'message' => "Registered successfully.",
+            'data' => compact('user', 'token')
+        ]);
     }
 
     public function login(): JsonResponse
